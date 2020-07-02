1419d01e80db1905b15b131e4bd8dec6ba510ba6
Rodrigo <cotarelorodrigo@gmail.com>
2020-07-02 11:35:31 -0300

    Init de threads

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::thread;
+
+const CANTIDAD_CARTAS: u8 = 52; //La baraja francesa es un conjunto de naipes o cartas, formado por 52 unidades.
+const N_JUGADORES: u8 = 6;
+
 fn main() {
-    println!("Hello, world!");
+    //let mut players = vec![];
+    println!("Cantidad de cartas: {}", CANTIDAD_CARTAS);
+
+    let mut children = vec![];
+
+    for i in 1..N_JUGADORES + 1 {
+    	children.push( thread::spawn(move || 
+    		{ 
+    			println!("Jugador {}: Tengo {} cartas", i, CANTIDAD_CARTAS/N_JUGADORES); 
+    		}
+    	));
+    }
+
+
+    for child in children {
+		// Esperar que terminen los threads
+		let _ = child.join();
+	}
+
 }
